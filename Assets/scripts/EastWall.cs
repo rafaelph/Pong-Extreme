3ca350e2ab8e4b113ea10d5253b6b6207475c66a
Rafael Delos Santos <rkdelossantos@gmail.com>
2017-04-17 20:26:01 +1000

    Moved scoring logic to walls

---
+++ Assets/scripts/EastWall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EastWall : MonoBehaviour {
+
+	public GameMaster gameMaster;
+
+	void OnCollisionEnter2D (Collision2D collider) {
+		GameObject colliderObject = collider.gameObject;
+		if (collider.gameObject.CompareTag ("ball")) {
+			gameMaster.onLeftPlayerScore ();
+		} 
+	}
+}
--- Assets/scripts/WestWall.cs
+++ Assets/scripts/WestWall.cs
@@ -1,22 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class BallScore : MonoBehaviour {
+public class WestWall : MonoBehaviour {
 
 	public GameMaster gameMaster;
 
-
 	void OnCollisionEnter2D (Collision2D collider) {
 		GameObject colliderObject = collider.gameObject;
-		if (collider.gameObject.CompareTag ("right_wall")) {
-			gameMaster.onLeftPlayerScore ();
-		} 
-
-		if (collider.gameObject.CompareTag ("left_wall")) {
+		if (collider.gameObject.CompareTag ("ball")) {
 			gameMaster.onRightPlayerScore ();
-		}
+		} 
 	}
-		
 }
