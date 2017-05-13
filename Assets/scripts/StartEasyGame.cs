9909eea4b51a1ba390282561496c588070039614
Rafael Delos Santos <rkdelossantos@gmail.com>
2017-05-14 00:44:34 +1000

    Added main menu and music in each scene

---
+++ Assets/scripts/StartEasyGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartEasyGame : MonoBehaviour {
+
+	private Text startText;
+
+	public GameObject loadingMask;
+
+	public void openNextScene() {
+		SceneManager.LoadScene ("Main Scene");
+		showLoadingScreen ();
+	}
+
+	private void showLoadingScreen() {
+		loadingMask.SetActive (true);
+	}
+
+
+}
