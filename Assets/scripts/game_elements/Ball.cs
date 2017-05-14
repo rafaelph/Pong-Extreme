fd865613040aa5e6a1308bdf59ccab270096a1d4
Rafael Delos Santos <rkdelossantos@gmail.com>
2017-05-14 13:28:32 +1000

    Added chance generator

--- Assets/scripts/game_elements/Ball.cs
+++ Assets/scripts/game_elements/Ball.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball	 : MonoBehaviour {
+public class Ball : MonoBehaviour {
 
 	public GameMaster gameMaster;
 	public int ballSpeed;
---
+++ Assets/scripts/util/ChanceGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class ChanceGenerator {
+
+	private static Random random = new Random ();
+
+	public static bool IsFailure(int successFailureOver100) {
+		return !ChanceGenerator.IsSuccessful (successFailureOver100);
+	}
+
+	public static bool IsSuccessful(int successPercentageOver100) {
+		if (successPercentageOver100 >= 100) {
+			return true;
+		} else if (successPercentageOver100 <= 0) {
+			return false;
+		} else {
+			ArrayList arrayOfPercentage = new ArrayList ();
+			for (var i = 0; i < successPercentageOver100; ++i) {
+				arrayOfPercentage.Add (true);
+			}
+			for (var i = 0; i < 100 - successPercentageOver100; ++i) {
+				arrayOfPercentage.Add (false);
+			}
+			return (bool)arrayOfPercentage[random.Next(99)];
+		}
+	}
+
+}
